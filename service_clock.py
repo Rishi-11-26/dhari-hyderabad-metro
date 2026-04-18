af0ce60ed4d3f61c14ba215a297da383db487080
Rishi-11-26 <rishipourusha@gmail.com>
2026-04-19 00:31:37 +0530

    Add timezone support for metro service status

--- service_clock.py
+++ service_clock.py
@@ -1,11 +1,10 @@
 from datetime import datetime
-
-
-from datetime import datetime
+import pytz
 
 def metro_service_status():
 
-    now = datetime.now()
+    india = pytz.timezone("Asia/Kolkata")
+    now = datetime.now(india)
 
     start_time = now.replace(hour=6, minute=0, second=0, microsecond=0)
     end_time = now.replace(hour=23, minute=0, second=0, microsecond=0)
